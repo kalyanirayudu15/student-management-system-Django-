8dae86ef82c332128ba2db57ca805b67b6d4cf01
Rayudu Kalyani <kalyanirayudu98@gmail.com>
2026-09-23 15:17:31 +0530

    Initial commit

---
+++ rattles/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RattlesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "rattles"
---
+++ rattles/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+from django.urls import reverse
+
+# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    identityNumber = models.CharField(max_length=200, null=False)
+    address = models.CharField(max_length=200, null=True)
+    department = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    aadhar = models.CharField(max_length=200, null=True)
+    blood_group = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+# The absolute path to get the url then reverse into 'student_edit' with keyword arguments (kwargs) primary key
+    def get_absolute_url(self):
+        return reverse('student_edit', kwargs={'pk': self.pk})
+
